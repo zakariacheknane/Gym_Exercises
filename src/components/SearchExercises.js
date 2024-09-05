491bd4f4dd42dc7677c390f2416a09fbeb4b109a
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-05 05:39:16 +0100

    Complete Search

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,8 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
-const SearchExercises = ({ setExercises }) => {
+import HorizontalScrollbar from './HorizontalScrollbar';
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState('');
+    const [bodyParts, setBodyParts] = useState([]);
+
+    useEffect(() => {
+      const fetchExercisesData = async () => {
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+  
+        setBodyParts(['all', ...bodyPartsData]);
+      };
+  
+      fetchExercisesData();
+    }, []);
 
     const handleSearch = async () => {
       if (search) {
@@ -35,6 +47,9 @@ const SearchExercises = ({ setExercises }) => {
            <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch} >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+      <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
       </Box>
       </Stack>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,12 @@ import Exercises from "../components/Exercises";
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+  
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={setExercises} />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       <Exercises />
     </Box>
   );
