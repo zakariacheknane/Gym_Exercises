f0931991efedf08262c1f1735487689388fe9565
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-06 04:58:30 +0100

    Finish ExerciseCard

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,14 +3,17 @@ import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises)
   return (
+    
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
        <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
        <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
        {exercises.map((exercise, idx) => (
-          exercise.name
+       <ExerciseCard key={idx} exercise={exercise} />
         ))}
         </Stack>
       </Box>
