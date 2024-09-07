989d2ea4c70ea3ee57db4d7d5336f7d218b33cc4
zakariacheknane <cheknanezakaria@gmail.com>
2024-09-07 04:52:19 +0100

    Add Loader

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
+import Loader from './Loader';
 
 function ExerciseVideos({ exerciseVideos, name }) {
+  if (!exerciseVideos.length) return <Loader />;
   return (
 <Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
 <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
